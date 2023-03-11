992fa4e96fc7259e6e3fa66f5598e28e4833f89b
Gandalf The Grey <mcavalcantirebola@gmail.com>
2023-03-11 16:24:33 -0300

    correção no H2 e pom.xml

--- src/main/java/br/com/fiap/checkpoint1/Checkpoint1Application.java
+++ src/main/java/br/com/fiap/checkpoint1/Checkpoint1Application.java
@@ -2,6 +2,11 @@ package br.com.fiap.checkpoint1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Checkpoint1Application {
